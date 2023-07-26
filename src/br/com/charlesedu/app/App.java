31c1f5928adeeb6d43d40bf1281552008daea05f
unknown <charleseduardofaria@gmail.com>
2023-07-26 14:33:32 -0300

    Criando classe Position

---
+++ src/br/com/charlesedu/app/App.java
@@ -0,0 +1,11 @@
+package br.com.charlesedu.app;
+
+import br.com.charlesedu.boardgame.Position;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Position position = new Position(3, 5);
+
+        System.out.println(position);
+    }
+}
