aff90b5552ff0a554b150007ff86241af919faa9
unknown <charleseduardofaria@gmail.com>
2023-07-26 16:51:28 -0300

    Criando peça e tabuleiro

--- src/br/com/charlesedu/app/App.java
+++ src/br/com/charlesedu/app/App.java
@@ -1,11 +1,10 @@
 package br.com.charlesedu.app;
 
+import br.com.charlesedu.boardgame.Board;
 import br.com.charlesedu.boardgame.Position;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position position = new Position(3, 5);
-
-        System.out.println(position);
+        Board board = new Board(8, 8);
     }
 }
