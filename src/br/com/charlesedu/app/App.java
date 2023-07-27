579fb207dff796733715ee63d9202d0eff650d93
unknown <charleseduardofaria@gmail.com>
2023-07-27 10:48:13 -0300

    Movendo as peças

--- src/br/com/charlesedu/app/App.java
+++ src/br/com/charlesedu/app/App.java
@@ -1,11 +1,29 @@
 package br.com.charlesedu.app;
 
+import java.util.Scanner;
+
 import br.com.charlesedu.chess.ChessMatch;
+import br.com.charlesedu.chess.ChessPiece;
+import br.com.charlesedu.chess.ChessPosition;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
         ChessMatch chessMatch = new ChessMatch();
 
-        UI.printBoard(chessMatch.getPieces());
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
     }
 }
