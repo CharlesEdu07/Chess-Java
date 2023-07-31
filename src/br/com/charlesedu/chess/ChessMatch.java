0f382b0c94aa27d7929c43ed502f0b71b09c7c62
unknown <charleseduardofaria@gmail.com>
2023-07-31 14:37:20 -0300

    Rainha e correção do peão

--- src/br/com/charlesedu/chess/ChessMatch.java
+++ src/br/com/charlesedu/chess/ChessMatch.java
@@ -10,6 +10,7 @@ import br.com.charlesedu.chess.pieces.Bishop;
 import br.com.charlesedu.chess.pieces.King;
 import br.com.charlesedu.chess.pieces.Knight;
 import br.com.charlesedu.chess.pieces.Pawn;
+import br.com.charlesedu.chess.pieces.Queen;
 import br.com.charlesedu.chess.pieces.Rook;
 
 public class ChessMatch {
@@ -219,6 +220,7 @@ public class ChessMatch {
         placeNewPiece('a', 1, new Rook(board, Color.WHITE));
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
@@ -235,6 +237,7 @@ public class ChessMatch {
         placeNewPiece('a', 8, new Rook(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
